b86c7a82623ab9068a62c26fce703161aaef152f
Dirk Baeumer <dirkb@microsoft.com>
2024-08-27 10:38:13 +0200

    Add second command

--- src/extension.ts
+++ src/extension.ts
@@ -20,6 +20,17 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable2 = vscode.commands.registerCommand('playground.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from playground!');
+	});
+
+	context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
